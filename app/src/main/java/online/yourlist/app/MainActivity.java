7843e11811b4ce90ae68e5c2b14b3e4f652eff49
SimDifRandomHD <contact@gfaundead.stream>
2023-07-06 21:05:04 +1000

    Create MainActivity.java

---
+++ app/src/main/java/online/yourlist/app/MainActivity.java
@@ -0,0 +1,19 @@
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://app.yourlist.online");
+    }
+}
